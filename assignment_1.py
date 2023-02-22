f9e543802c1df74df1967216b3428f82ae513d8a
Andreas Gangstad Rygg <andreryg@stud.ntnu.no>
2023-02-22 23:40:30 +0100

    Added covariance matrix calculations

--- assignment_1.py
+++ assignment_1.py
@@ -26,23 +26,42 @@ longitude = 10.43443358
 latitu = 63.42730197
 longitu = 10.38034967
 
+test1 = 63.40919533
+test2 = 10.58311814
+
+#ST46-TP342 correlation matrix
+std_st46_tp342 = [0.0002, 0.0001, 0.0004]
+K_st46_tp342 = np.array([[1.0000, 0.2226, 0.2668],
+                         [0.2226, 1.0000, 0.2386], 
+                         [0.2668, 0.2386, 1.0000]])
 
 
-def transformation_matrix(latitude, longitude, dx, dy, dz):
+def transformation_matrix(latitude, longitude):
     """
-    Calculates delta x, delta y and delta z in a local geodetic system based on a cartesian baseline and the latitude and longitude of the starting point.
+    Calculates the transformation matrix required to transform a point defined by latitude and longitude to a local geodetic system.
     :param latitude: float
     :param longitude: float
+    :return: 2d numpy array
+    """
+    latitude = np.deg2rad(latitude)
+    longitude = np.deg2rad(longitude)
+    Trans = np.array([[-np.sin(latitude)*np.cos(longitude), -np.sin(latitude)*np.sin(longitude), np.cos(latitude)],
+                      [-np.sin(longitude), np.cos(longitude), 0], 
+                        [np.cos(latitude)*np.cos(longitude), np.cos(latitude)*np.sin(longitude), np.sin(latitude)]])
+    print(Trans)
+    return Trans
+
+def transform(T, dx, dy, dz):
+    """
+    Transform a baseline to a local geodetic system.
+    :param T: transformation matrix
     :param dx: float
     :param dy: float
     :param dz: float
     :return: numpy array
     """
-    point = np.array([dx,dy,dz]).T
-    Trans = np.array([[-np.sin(latitude)*np.cos(longitude), -np.sin(latitude)*np.sin(longitude), np.cos(latitude)],
-                      [-np.sin(longitude), np.cos(longitude), 0], 
-                        [np.cos(latitude)*np.cos(longitude), np.cos(latitude)*np.sin(longitude), np.sin(latitude)]])
-    return np.dot(Trans,point)
+    point = np.array([[dx],[dy],[dz]])
+    return np.dot(T, point)
 
 def slope_distance(dx, dy, dz):
     return np.sqrt(dx**2+dy**2+dz**2)
@@ -51,13 +70,26 @@ def horizontal_distane(dx, dy):
     return np.sqrt(dx**2+dy**2)
 
 def azimuth(dx, dy):
-    return np.arctan(dy/dx)
+    return np.rad2deg(np.arctan(dy/dx))
+
+def covariance_matrix(std, K):
+    std_matrix = np.array([[std[0], 0, 0],
+                             [0, std[1], 0], 
+                             [0, 0, std[2]]])
+    return np.dot(np.dot(std_matrix, K), std_matrix)
     
 
-local_dx1, local_dy1, local_dz1 = transformation_matrix(latitude, longitude, dx1, dy1, dz1)
-local_dx2, local_dy2, local_dz2 = transformation_matrix(latitude, longitude, dx2, dy2, dz2)
-print(local_dz1, local_dz2)
-#print(transformation_matrix(latitude, longitude, dx2, dy2, dz2))
-print(horizontal_distane(local_dx1, local_dy1))
-print(azimuth(local_dx1, local_dy1))
+if __name__ == "__main__":
+    local_dx1, local_dy1, local_dz1 = transform(transformation_matrix(latitude, longitude), dx1, dy1, dz1)
+    local_dx2, local_dy2, local_dz2 = transform(transformation_matrix(latitude, longitude), dx2, dy2, dz2)
+    print(local_dx1, local_dy1)
+    print(local_dz1, local_dz2)
+    #print(transformation_matrix(latitude, longitude, dx2, dy2, dz2))
+    print(horizontal_distane(local_dx1, local_dy1))
+    print(azimuth(local_dx1, local_dy1))
+    print(azimuth(local_dx2, local_dy2))
 
+
+    transform(transformation_matrix(test1, test2), dx1, dy1, dz1)
+
+    print(covariance_matrix(std_st46_tp342, K_st46_tp342))
