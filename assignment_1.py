18ad7a61ccf43e1f4b76690e454172179cd483d2
Andreas Gangstad Rygg <andreryg@stud.ntnu.no>
2023-03-07 13:31:07 +0100

    Task 2a

--- assignment_1.py
+++ assignment_1.py
@@ -32,6 +32,10 @@ test2 = 10.58311814
 a = 6378137
 b = 6356752.3141
 
+N_moholt = 170.839 - 131.404
+N_tp342 = 44.618 - 5.194
+N_st46 = 151.851 - 112.554
+
 #test_dx = 
 
 #ST46-TP342 correlation matrix
@@ -39,8 +43,10 @@ std_st46_tp342 = [0.0002, 0.0001, 0.0004]
 K_st46_tp342 = np.array([[1.0000, 0.2226, 0.2668],
                          [0.2226, 1.0000, 0.2386], 
                          [0.2668, 0.2386, 1.0000]])
-
-
+std_st46_moholt = [0.0001, 0.0001, 0.0003]
+K_std46_moholt = np.array([[1.0000, 0.3026, 0.3727],
+                           [0.3026, 1.0000, 0.2288],
+                           [0.3727, 0.2288, 1.0000]])
 def transformation_matrix(latitude, longitude):
     """
     Calculates the transformation matrix required to transform a point defined by latitude and longitude to a local geodetic system.
@@ -86,12 +92,18 @@ def azimuth(dx, dy):
         return np.arctan(dy/dx)*200/np.pi + 200 
     else: #Zone 4
         return -np.arctan(dy/dx)*200/np.pi + 400
+    
+def zenith(dx, dy, dz):
+    """
+    Calculates zenith angle in gradian between two points.
+    """
+    return np.arctan(horizontal_distane(dx,dy)/dz)*200/np.pi
 
 def covariance_matrix(std, K):
     std_matrix = np.array([[std[0], 0, 0],
                              [0, std[1], 0], 
                              [0, 0, std[2]]])
-    return np.dot(np.dot(std_matrix, K), std_matrix)
+    return std_matrix @ K @ std_matrix
 
 def distance_UTM(Shh, R, Hs, z, ya, yb):
     S0 = R * np.arctan(Shh / (R + Hs + z))
@@ -123,6 +135,13 @@ def UTM_bearing(latitude, longitude, R, xa, xb, ya, yb, A, a = 6378137, b = 6356
 
     return bearing
 
+def NN2000_height_diff(zv, dx, dy, dz, N1, N2, R):
+    dN = N2 - N1
+    dHE = (slope_distance(dx, dy, dz) * np.sin(zv*np.pi/200))**2 / (2*R)
+
+    dH = dz - dN + dHE
+    return dH
+
 def main():
     #TASK 1
     print(f"----------------------Task 1a----------------------")
@@ -142,18 +161,22 @@ def main():
     print(f"----------------------Task 1b----------------------")
     utm_dist_1 = distance_UTM(horizontal_distane(local_dx1, local_dy1), radius(latitude, azimuth(local_dx1, local_dy1)), slope_distance(local_dx1, local_dy1, local_dz1), local_dz1, 0, local_dy1)
     utm_dist_2 = distance_UTM(horizontal_distane(local_dx2, local_dy2), radius(latitude, azimuth(local_dx2, local_dy2)), slope_distance(local_dx2, local_dy2, local_dz2), local_dz2, 0, local_dy2)
-    print(f"ST46-TP342 UTM distance: {utm_dist_1[0]} m, compared to values from table 2: {np.sqrt((7033941.628-7034487.402)**2 + (568890.318 - 571578.304)**2)} m, Difference: {utm_dist_1[0] - np.sqrt((7033941.628-7034487.402)**2 + (568890.318 - 571578.304)**2)} m.")
-    print(f"ST46-MOHOLT UTM distance: {utm_dist_2[0]} m, compared to values from table 2: {np.sqrt((7031952.892-7034487.402)**2 + (571469.041 - 571578.304)**2)} m, Difference: {utm_dist_2[0] - np.sqrt((7031952.892-7034487.402)**2 + (571469.041 - 571578.304)**2)} m.")
+    print(f"ST46-TP342 UTM distance: {utm_dist_1[0]} m, compared to values from table 2: {np.sqrt((7033941.628-7034487.402)**2 + (568890.318 - 571578.304)**2)} m. Difference: {utm_dist_1[0] - np.sqrt((7033941.628-7034487.402)**2 + (568890.318 - 571578.304)**2)} m.")
+    print(f"ST46-MOHOLT UTM distance: {utm_dist_2[0]} m, compared to values from table 2: {np.sqrt((7031952.892-7034487.402)**2 + (571469.041 - 571578.304)**2)} m. Difference: {utm_dist_2[0] - np.sqrt((7031952.892-7034487.402)**2 + (571469.041 - 571578.304)**2)} m.")
 
     #TASK 1c
     print(f"----------------------Task 1c----------------------")
     bearing_1 = UTM_bearing(latitude, longitude, radius(latitude, azimuth(local_dx1, local_dy1)), 0, local_dx1, 0, local_dy1, azimuth(local_dx1, local_dy1))
     bearing_2 = UTM_bearing(latitude, longitude, radius(latitude, azimuth(local_dx2, local_dy2)), 0, local_dx2, 0, local_dy2, azimuth(local_dx2, local_dy2))
-    print(f"ST46-TP342 bearing: {bearing_1[0]} grad, compared to values from table 2: {azimuth(7033941.628-7034487.402, 568890.318 - 571578.304)} grad, Difference: {bearing_1[0] - azimuth(7033941.628-7034487.402, 568890.318 - 571578.304)} grad.")
-    print(f"ST46-MOHOLT bearing: {bearing_2[0]} grad, compared to values from table 2: {azimuth(7031952.892-7034487.402, 571469.041 - 571578.304)} grad, Difference: {bearing_2[0] - azimuth(7031952.892-7034487.402, 571469.041 - 571578.304)} grad.")
+    print(f"ST46-TP342 bearing: {bearing_1[0]} grad, compared to values from table 2: {azimuth(7033941.628-7034487.402, 568890.318 - 571578.304)} grad. Difference: {bearing_1[0] - azimuth(7033941.628-7034487.402, 568890.318 - 571578.304)} grad.")
+    print(f"ST46-MOHOLT bearing: {bearing_2[0]} grad, compared to values from table 2: {azimuth(7031952.892-7034487.402, 571469.041 - 571578.304)} grad. Difference: {bearing_2[0] - azimuth(7031952.892-7034487.402, 571469.041 - 571578.304)} grad.")
 
     #TASK 1d
-    print(f"----------------------Task 1c----------------------")
+    print(f"----------------------Task 1d----------------------")
+    NN2000_diff_1 = NN2000_height_diff(zenith(local_dx1, local_dy1, local_dz1), local_dx1, local_dy1, local_dz1, N_st46, N_tp342, radius(latitude, azimuth(local_dx1, local_dy1)))
+    NN2000_diff_2 = NN2000_height_diff(zenith(local_dx2, local_dy2, local_dz2), local_dx2, local_dy2, local_dz2, N_st46, N_moholt, radius(latitude, azimuth(local_dx2, local_dy2)))
+    print(f"ST46-TP342 NN2000 height difference: {NN2000_diff_1[0]} m, compared to values from table 2: {5.194-112.554} m. Difference: {NN2000_diff_1[0] - (5.194-112.554)}")
+    print(f"ST46-MOHOLT NN2000 height difference: {NN2000_diff_2[0]} m, compared to values from table 2: {131.404-112.554} m. Difference: {NN2000_diff_2[0] - (131.404-112.554)}")
     #print(transformation_matrix(latitude, longitude, dx2, dy2, dz2))
     #print(distance_UTM((horizontal_distane(local_dx1, local_dy1)), , )
     #print(azimuth(local_dx1, local_dy1))
@@ -162,7 +185,11 @@ def main():
 
     #transform(transformation_matrix(test1, test2), dx1, dy1, dz1)
 
-    #print(covariance_matrix(std_st46_tp342, K_st46_tp342))
+    print(f"----------------------Task 2a----------------------")
+    covariance_1 = covariance_matrix(std_st46_tp342, K_st46_tp342)
+    covariance_2 = covariance_matrix(std_st46_moholt, K_std46_moholt)
+    print(f"Variance/Covariance matrix of st46-tp342 baseline: \n {covariance_1}")
+    print(f"Variance/Covariance matrix of st46-moholt baseline: \n {covariance_2}")
 
 
 if __name__ == "__main__":
